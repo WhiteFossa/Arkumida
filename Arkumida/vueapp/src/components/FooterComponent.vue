421dd8806286e20dbdee4ed70d136ecbe1b91f45
White Fossa <whitefossa@protonmail.com>
2023-05-16 22:47:33 +0200

    Footer in progress, ready for site friends

--- Arkumida/vueapp/src/components/FooterComponent.vue
+++ Arkumida/vueapp/src/components/FooterComponent.vue
@@ -1,6 +1,7 @@
 ﻿<!-- Site footer -->
 <script setup>
     import VersionInfo from './VersionInfo.vue'
+    import SiteFriends from "./SiteFriends.vue";
 
 </script>
 
@@ -9,23 +10,58 @@
         <div class="horizontal-flex-center">
             
             <div class="footer-info-column">
-                Info
+                <div>
+                    furtails-logo
+                </div>
+                <div>
+                    
+                </div>
             </div>
 
             <div class="footer-useful-column">
-                Useful
+                <div class="footer-column-header">
+                    Полезное
+                </div>
+                <div>
+                    Useful
+                </div>
             </div>
             
             <div class="footer-developers-column">
-                Developers
+                <div class="footer-column-header">
+                    Разработчики
+                </div>
+                <div>
+                    <div class="footer-block">
+                        Изначальный дизайн и php-программирование - Şeti
+                    </div>
+
+                    <div class="footer-block">
+                        Редизайн, программирование - <a href="https://fossa.life" title="Блог Белой Фоссы">Белая Фосса</a>
+                    </div>
+
+                    <div class="footer-block">
+                        Идея сайта и перевод текстов - <a href="mailto:redgerra@rambler.ru">Redgerra</a>
+                    </div>
+                </div>
             </div>
             
             <div class="footer-friends-column">
-                Friends
+                <div class="footer-column-header">
+                    Друзья сайта
+                </div>
+                <div>
+                    <SiteFriends />
+                </div>
             </div>
             
             <div class="footer-partners-column">
-                Partners
+                <div class="footer-column-header">
+                    Реклама
+                </div>
+                <div>
+                    Useful
+                </div>
             </div>
         </div>
 
---
+++ Arkumida/vueapp/src/components/SiteFriends.vue
@@ -0,0 +1,42 @@
+<!-- Shows site friends list -->
+<script setup>
+    import LoadingSymbol from './LoadingSymbol.vue'
+    
+    import { ref, onMounted } from 'vue'
+    
+    // True if loading under way
+    const isLoading = ref(true)
+
+    const friends = ref([
+        { id: '1da68e2d-8e11-485f-a40e-78a4414e8792', text: 'Фосса', title: 'Блог Белой Фоссы', url: 'https://fossa.life' },
+        { id: '1da68e2d-8e11-485f-a40e-78a4414e8793', text: 'Aaz', title: 'YiffYuff.RU', url: 'https://yiffyuff.ru' },
+    ])
+    
+    // OnMounted hook
+    onMounted(async () =>
+    {
+      await OnLoad();
+    })
+    
+    // Called when page is loaded
+    async function OnLoad()
+    {
+      //const versionInfo = await (await fetch(`api/VersionInfo/Get`)).json()
+      //versionString.value = versionInfo.versionString
+    
+      isLoading.value = false
+    }
+
+</script>
+
+<template>
+    <div v-if="isLoading">
+        <LoadingSymbol />
+    </div>
+    <div v-else>
+        <!-- Shown after load -->
+        <div class="footer-block" v-for="friend in friends" :key="friend.id">
+            <a :href="friend.url" :title="friend.title">{{ friend.text }}</a>
+        </div>
+    </div>
+</template>
