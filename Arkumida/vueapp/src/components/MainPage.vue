7d0fab6881b375e2ce60ef63815601265760881c
White Fossa <whitefossa@protonmail.com>
2023-05-27 05:48:30 +0200

    Added top buttons, switched to rems from pixels

--- Arkumida/vueapp/src/components/MainPage.vue
+++ Arkumida/vueapp/src/components/MainPage.vue
@@ -1,10 +1,20 @@
 <script setup>
 
+import TopButton from "@/components/TopButton.vue";
+
 </script>
 
 <template>
     <body>
         <!-- Main page code -->
+        
+        <!-- Top buttons -->
+        <div class="horizontal-flex top-buttons-container">
+            <TopButton href="/addText" text="Добавить"/>
+            <TopButton href="/collectiveTranslation" text="Совместный перевод" smallText="Сейчас переводятся: 2"/>
+            <TopButton href="/contests" text="Конкурсы" />
+        </div>
+        
         <h1>Furtails.pw</h1>
     </body>
 </template>
---
+++ Arkumida/vueapp/src/components/TopButton.vue
@@ -0,0 +1,24 @@
+<!-- Shows top button -->
+<script setup>
+
+    import { defineProps } from 'vue'
+    
+    const props = defineProps({
+        text: String,
+        smallText: { type: String, default: "" },
+        href: String
+    })
+
+</script>
+
+<template>
+    <a :href="href">
+        <div v-if="props.smallText !== ''" class="top-button top-button-with-small-text">
+            {{ props.text }}
+            <div class="top-button-small-text">{{ props.smallText }}</div>
+        </div>
+        <div v-else class="top-button">
+            {{ props.text }}
+        </div>
+    </a>
+</template>
