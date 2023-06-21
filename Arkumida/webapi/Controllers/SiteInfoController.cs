993b162c776865cc9cf731819e64a0e2bc691ce6
White Fossa <whitefossa@protonmail.com>
2023-06-22 00:06:09 +0200

    Backend version bump

--- Arkumida/webapi/Controllers/SiteInfoController.cs
+++ Arkumida/webapi/Controllers/SiteInfoController.cs
@@ -16,7 +16,7 @@ public class SiteInfoController : ControllerBase
     [HttpGet]
     public async Task<ActionResult<VersionInfoResponse>> GetVersionInfoAsync()
     {
-        return Ok(new VersionInfoResponse("Аркумида-А мод. 14", "https://github.com/WhiteFossa/Arkumida"));
+        return Ok(new VersionInfoResponse("Аркумида-А мод. 15", "https://github.com/WhiteFossa/Arkumida"));
     }
     
     /// <summary>
