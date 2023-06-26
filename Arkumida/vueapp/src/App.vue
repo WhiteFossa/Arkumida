a35a005508cecb8810d475e2dbcb3ac1fe677808
White Fossa <whitefossa@protonmail.com>
2023-06-26 14:28:17 +0200

    Routing support

--- Arkumida/vueapp/src/App.vue
+++ Arkumida/vueapp/src/App.vue
@@ -1,13 +1,3 @@
-<script setup>
-    import MainPage from './components/MainPage.vue'
-    import FooterComponent from './components/FooterComponent.vue'
-    import HeaderComponent from "@/components/HeaderComponent.vue";
-</script>
-
 <template>
-    <HeaderComponent />
-    
-    <MainPage />
-    
-    <FooterComponent />
+  <router-view/>
 </template>
---
+++ Arkumida/vueapp/src/components/ReadTextComponent.vue
@@ -0,0 +1,11 @@
+<!-- Root component for reading texts -->
+
+<script setup>
+
+</script>
+
+<template>
+    <h1>Root component for reading texts</h1>
+
+    <div>Text ID: {{ $route.params.id }}</div>
+</template>
