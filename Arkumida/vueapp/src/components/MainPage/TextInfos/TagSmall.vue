8464ef63fda0819c2088581f33db777e81a6b15a
White Fossa <whitefossa@protonmail.com>
2023-10-28 07:48:32 +0200

    Clickable tags and categories, everywhere!

--- Arkumida/vueapp/src/components/MainPage/TextInfos/TagSmall.vue
+++ Arkumida/vueapp/src/components/MainPage/TextInfos/TagSmall.vue
@@ -1,24 +1,21 @@
 <!-- Small tag (color is fixed) -->
 <script setup>
-
     import { ref, defineProps } from 'vue'
+    import {GenerateOneTagSearchQuery} from "@/js/libArkumida";
     
     const props = defineProps({
-        id: String,
-        furryReadableId: String,
-        text: String
+        tag: Object
     })
     
     const tagHref = ref(null)
     const tagTitle = ref(null)
     
-    tagHref.value = "/texts/byTag/" + props.id
-    tagTitle.value = "Все произведения с тегом " + props.text;
-
+    tagHref.value = GenerateOneTagSearchQuery(props.tag.tag)
+    tagTitle.value = "Все произведения с тегом " + props.tag.tag
 </script>
 
 <template>
     <a class="small-tag-link" :href="tagHref" :title="tagTitle">
-        {{ props.text }}
+        {{ props.tag.tag }}
     </a>
 </template>
