c06a606b7407a1d14eba4ddde58b00e1e5bbd194
White Fossa <whitefossa@protonmail.com>
2023-11-25 12:28:39 +0100

    Dislikes display settings in CreatureProfileDbo

---
+++ Arkumida/webapi/Dao/Migrations/20231125004542_DislkiesSettings.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapi.Migrations.SecurityDb
+{
+    /// <inheritdoc />
+    public partial class DislkiesSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VotesCount",
+                table: "Texts");
+
+            migrationBuilder.DropColumn(
+                name: "VotesMinus",
+                table: "Texts");
+
+            migrationBuilder.DropColumn(
+                name: "VotesPlus",
+                table: "Texts");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsShowDislikes",
+                table: "Profiles",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsShowDislikesAuthors",
+                table: "Profiles",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
