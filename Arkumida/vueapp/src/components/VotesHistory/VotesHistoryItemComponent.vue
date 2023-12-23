6ebe3ffddc6469b8d6b9f5ca1f63f0e9d57e8d0a
White Fossa <whitefossa@protonmail.com>
2023-12-23 01:39:48 +0100

    Working (but not ready yet) votes history

--- Arkumida/vueapp/src/components/VotesHistory/VotesHistoryItemComponent.vue
+++ Arkumida/vueapp/src/components/VotesHistory/VotesHistoryItemComponent.vue
@@ -1,6 +1,8 @@
 <script setup>
     import {defineProps} from "vue";
     import moment from "moment/moment";
+    import {AvatarClass, Messages} from "@/js/constants";
+    import AvatarComponent from "@/components/Shared/AvatarComponent.vue";
 
     const props = defineProps({
         vote: Object
@@ -10,16 +12,29 @@
 <template>
     <div class="votes-history-item-container">
 
-        <div>{{ moment(props.vote.timestamp).format('HH:mm DD.MM.YYYY') }}</div>
+        <div class="votes-history-item-timestamp">{{ moment(props.vote.timestamp).format('HH:mm DD.MM.YYYY') }}</div>
 
         <!-- Creature -->
-        <div v-if="props.vote.isCreatureHidden">Скрыто</div>
-        <div v-if="!props.vote.isCreatureHidden">{{ props.vote.creature.name }}</div>
+        <div class="votes-history-item-creature">
+
+            <div v-if="props.vote.isCreatureHidden">
+                <AvatarComponent :avatar="null" :avatarClass="AvatarClass.Small" />
+                <span class="creatures-info-link">Скрыто</span>
+            </div>
+
+            <div v-if="!props.vote.isCreatureHidden">
+                <a :href="'/users/' + props.vote.creature.entityId" :title="Messages.CreatureUser + props.vote.creature.title" target="_blank">
+                    <AvatarComponent :avatar="props.vote.creature.currentAvatar" :avatarClass="AvatarClass.Small" />
+                </a>
+                <a class="creatures-info-link" :href="'/users/' + props.vote.creature.entityId" :title="Messages.CreatureUser + props.vote.creature.title" target="_blank">{{ props.vote.creature.name }}</a>
+            </div>
+
+        </div>
 
         <!-- Vote -->
-        <div v-if="props.vote.type === 1">Голосует: "Произведение понравилось"</div>
-        <div v-if="props.vote.type === 2">Голосует: "Произведение не понравилось"</div>
-        <div v-if="props.vote.type === 4">Отзывает голос: "Произведение понравилось"</div>
-        <div v-if="props.vote.type === 5">Отзывает голос: "Произведение не понравилось"</div>
+        <div v-if="props.vote.type === 1">Голосует: "Понравилось"</div>
+        <div v-if="props.vote.type === 2">Голосует: "Не понравилось"</div>
+        <div v-if="props.vote.type === 4">Отзывает голос: "Понравилось"</div>
+        <div v-if="props.vote.type === 5">Отзывает голос: "Не понравилось"</div>
     </div>
 </template>
